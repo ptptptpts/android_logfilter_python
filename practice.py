5897ae91ed565f1eaf94d4959c23a6b521a47c1f
ptptptpts <ptptptpts@gmail.com>
2018-05-29 00:12:58 +0900

    Execute logcat on Thread and print it
    Signed-off-by: ptptptpts <ptptptpts@gmail.com>

--- practice.py
+++ practice.py
@@ -1,10 +1,24 @@
-import subprocess
 import datetime
+import threading
+import os
+import time
 
+def getLogcat(filename):
+    print("Run logcat on daemon thread")
+    os.system('adb logcat > ' + filename)
 
-def getLogcat():
-    filename = 'logcat_' + datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-    subprocess.call('adb logcat > ' + filename + '.txt', shell=True)
+def printLog(filename):
+    print("Print logs in " + filename)
+    f = open(file=filename, mode='r')
+    while True:
+        line = f.readline()
+        if not line: break
+        print(line)
+    f.close()
+
 
 if __name__ == '__main__':
-    getLogcat()
+    filename = 'logcat_' + datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '.txt'
+    tLogcat = threading.Thread(target=getLogcat, daemon=True, args=(filename,))
+    tLogcat.start()
+    printLog(filename)
